9751031ace6142931ae8afd95825fb8cc4804792
7Phone <in_fq@foxmail.com>
2026-09-11 10:35:01 +0800

    refactor: remove unused Bluetooth auto-connect option

--- src/BTTimeSync.Console/Configuration/AppConfig.cs
+++ src/BTTimeSync.Console/Configuration/AppConfig.cs
@@ -9,9 +9,4 @@ public sealed class AppConfig
     /// 时间同步相关配置。
     /// </summary>
     public TimeSyncOptions TimeSync { get; set; } = new();
-
-    /// <summary>
-    /// 蓝牙相关配置。
-    /// </summary>
-    public BluetoothOptions Bluetooth { get; set; } = new();
 }
